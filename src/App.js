9616ef721a9d49565b6326c880459c5a508785c5
tejasganvit78 <137061096+tejasganvit78@users.noreply.github.com>
2024-02-23 20:18:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import AddUser from "./components/AddUser";
+import Card from "./components/UI/Card";
+
+function App() {
+
+  return (
+    <div className="App">
+      <AddUser/>
+      <Card/>
+    </div>
+  );
+}
+export default App;
